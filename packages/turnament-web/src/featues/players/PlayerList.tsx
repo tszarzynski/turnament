e355116061d977099f83d5c2f7b5ef6c9ce66baa
Tomasz Szarzynski <tszarzynski@gmail.com>
2020-02-19 18:41:26 +0000

    Adding dragging

--- packages/turnament-web/src/featues/players/PlayerList.tsx
+++ packages/turnament-web/src/featues/players/PlayerList.tsx
@@ -1,9 +1,57 @@
-import { List } from "@material-ui/core";
-import React from "react";
+import { List, makeStyles } from "@material-ui/core";
+import React, { useEffect, useRef, useState, useLayoutEffect } from "react";
+import { animated, interpolate, useSprings } from "react-spring";
+import { useDrag } from "react-use-gesture";
+import { Player } from "turnament-scheduler";
 import PlayerForm from "./PlayerForm";
 import PlayerListItem from "./PlayerListItem";
-import { Player } from "turnament-scheduler";
-import { useTransition, animated } from "react-spring"
+
+// Returns fitting styles for dragged/idle items
+const fn = (
+  order: number[],
+  down: boolean = false,
+  originalIndex: number,
+  curIndex: number,
+  y: number,
+  originalHeight: number
+) => (index: number) =>
+  down && index === originalIndex
+    ? {
+        y: curIndex * originalHeight + y,
+        scale: 1.1,
+        zIndex: 1,
+        shadow: 15,
+        immediate: (n: string) => n === "y" || n === "zIndex"
+      }
+    : {
+        y: order.indexOf(index) * originalHeight,
+        scale: 1,
+        zIndex: 0,
+        shadow: 1,
+        immediate: (n: string) => false
+      };
+
+const clamp = (n: number, lower: number, upper: number) =>
+  Math.min(Math.max(n, lower), upper);
+const move = (arr: number[], from: number, to: number) => {
+  const newArr = arr.slice();
+
+  newArr.splice(to, 0, newArr.splice(from, 1)[0]);
+
+  return newArr;
+};
+
+const useStyles = makeStyles(theme => ({
+  list: {
+    position: "relative",
+    userSelect: "none"
+  },
+  listItem: {
+    position: "absolute",
+    width: "100%",
+    transformOrigin: "50% 50% 0px"
+  }
+}));
 
 interface IProps {
   players: Player[];
@@ -16,25 +64,79 @@ export default function PlayerList({
   addPlayer,
   removePlayer
 }: IProps) {
+  const classes = useStyles();
+  const order = useRef<number[]>([]);
+  const itemsRef = useRef<HTMLElement[]>([]);
+  const [items, setItems] = useState<Player[]>(players);
+  const [listHeight, setListHeight] = useState<number>(0);
 
-  const transitions = useTransition(players, player => player.ID, {
-    from: { transform: 'translate3d(0,-40px,0)' },
-    enter: { transform: 'translate3d(0,0px,0)' },
-    leave: { transform: 'translate3d(0,-40px,0)' },
-  })
+  useEffect(() => {
+    setItems(players);
 
+    const idxs = players.map((_, index) => index);
 
+    order.current = [...order.current, ...idxs.slice(order.current.length)];
+    console.log(order.current);
+    itemsRef.current = new Array(players.length);
+  }, [players]);
+
+  const [springs, setSprings] = useSprings(
+    items.length,
+    fn(order.current, false, 0, 0, 0, 0)
+  );
+
+  useLayoutEffect(() => {
+    if (itemsRef.current.length) {
+      setSprings(
+        // @ts-ignore
+        fn(order.current, false, 0, 0, 0, itemsRef.current[0].offsetHeight)
+      );
+
+      setListHeight(itemsRef.current[0].offsetHeight * itemsRef.current.length);
+    }
+  }, [itemsRef, setSprings]);
+
+  const bind = useDrag(({ args: [originalIndex], down, movement: [, y] }) => {
+    const target = itemsRef.current[originalIndex];
+    const targetHeight = target.offsetHeight;
+    const curIndex = order.current.indexOf(originalIndex);
+    const curRow = clamp(
+      Math.round((curIndex * targetHeight + y) / targetHeight),
+      0,
+      items.length - 1
+    );
+    const newOrder = move(order.current, curIndex, curRow);
+
+    const newFn = fn(newOrder, down, originalIndex, curIndex, y, targetHeight);
+    // @ts-ignore
+    setSprings(newFn); // Feed springs new style data, they'll animate the view without causing a single render
+    if (!down) order.current = newOrder;
+  });
 
   return (
     <div>
-      <List>
-        {transitions.map(({ item, props, key }) =>
-          <animated.div key={key} style={props}>
-            <PlayerListItem
-              player={item}
-              removePlayer={removePlayer}
-            />
-          </animated.div>)}
+      <List className={classes.list} style={{ height: listHeight }}>
+        {springs.map(({ zIndex, shadow, y, scale }, i) => (
+          <animated.div
+            ref={el => (itemsRef.current[i] = el!)}
+            {...bind(i)}
+            key={i}
+            className={classes.listItem}
+            style={{
+              zIndex,
+              boxShadow: shadow.interpolate(
+                s => `rgba(0, 0, 0, 0.15) 0px ${s}px ${2 * s}px 0px`
+              ),
+              transform: interpolate(
+                [y, scale],
+                (y, s) => `translate3d(0,${y}px,0) scale(${s})`
+              )
+            }}
+            children={
+              <PlayerListItem player={items[i]} removePlayer={removePlayer} />
+            }
+          />
+        ))}
       </List>
       <PlayerForm addPlayer={addPlayer} />
     </div>
