e1447d81edad4aee56bd64f12a9d37deb2f7e54a
tszarzynski <tszarzynski@gmail.com>
2020-02-21 16:29:39 +0000

    Cleaning styles

--- packages/turnament-web/src/App.tsx
+++ packages/turnament-web/src/App.tsx
@@ -5,23 +5,13 @@ import "./App.css";
 import { getCurrentRoute, listen } from "./app/router";
 import { Page } from "./components/Page";
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "flex-start",
-    flexWrap: "wrap"
-  }
-}));
-
 const App: React.FC = () => {
-  const classes = useStyles();
   const [route, setRoute] = useState(getCurrentRoute());
 
   useEffect(() => listen(setRoute), [route]);
 
   return (
-    <Container component="main" maxWidth="sm" className={classes.paper}>
+    <Container component="main" maxWidth="sm">
       <Page route={route} />
     </Container>
   );
