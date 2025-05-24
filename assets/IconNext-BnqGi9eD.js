ed3aa365f76bf25432c0c29d89a99f019132ce34
Tomasz Szarzynski <tszarzynski@gmail.com>
2025-05-24 16:28:45 +0100

    Updates

---
+++ assets/IconNext-BnqGi9eD.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CiWGNNki.js";const r=()=>o.jsx("svg",{className:"w-10 h-10",xmlns:"http://www.w3.org/2000/svg",width:"24",height:"24",fill:"none",viewBox:"0 0 24 24",stroke:"currentColor","aria-label":"Next icon",role:"img",children:o.jsx("path",{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"1",d:"M17 8l4 4m0 0l-4 4m4-4H3"})});export{r as I};
